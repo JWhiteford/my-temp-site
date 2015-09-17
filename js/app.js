6e73eece1c926b470fb5527b468e5881b1ecb44d
Jeremy <jeremy@hybridforge.com>
2015-09-17 14:46:34 -0600

    added test page for rotating features

--- js/app.js
+++ js/app.js
@@ -11,7 +11,6 @@ jQuery(document).ready(function(event){
     if( !isAnimating ) changePage(newPage, true);
     firstLoad = true;
   });
-
   //detect the 'popstate' event - e.g. user clicking the back button
   $(window).on('popstate', function() {
     if( firstLoad ) {
@@ -72,49 +71,160 @@ jQuery(document).ready(function(event){
   function transitionsSupported() {
     return $('html').hasClass('csstransitions');
   }
-});
 
 
-$('.form').find('input, textarea').on('keyup blur focus', function (e) {
 
-  var $this = $(this),
-      label = $this.prev('label');
+  // form fancy labels
+  $('.form').find('input, textarea').on('keyup blur focus', function (e) {
 
-    if (e.type === 'keyup') {
-      if ($this.val() === '') {
+    var $this = $(this),
+        label = $this.prev('label');
+
+      if (e.type === 'keyup') {
+        if ($this.val() === '') {
+            label.removeClass('active highlight');
+          } else {
+            label.addClass('active highlight');
+          }
+      } else if (e.type === 'blur') {
+        if( $this.val() === '' ) {
           label.removeClass('active highlight');
         } else {
-          label.addClass('active highlight');
+          label.removeClass('highlight');
         }
-    } else if (e.type === 'blur') {
-      if( $this.val() === '' ) {
-        label.removeClass('active highlight');
-      } else {
-        label.removeClass('highlight');
-      }
-    } else if (e.type === 'focus') {
+      } else if (e.type === 'focus') {
 
-      if( $this.val() === '' ) {
-        label.removeClass('highlight');
+        if( $this.val() === '' ) {
+          label.removeClass('highlight');
+        }
+        else if( $this.val() !== '' ) {
+          label.addClass('highlight');
+        }
       }
-      else if( $this.val() !== '' ) {
-        label.addClass('highlight');
-      }
+  });
+
+  $('.tab a').on('click', function (e) {
+
+    e.preventDefault();
+
+    $(this).parent().addClass('active');
+    $(this).parent().siblings().removeClass('active');
+
+    target = $(this).attr('href');
+
+    $('.tab-content > div').not(target).hide();
+
+    $(target).fadeIn(600);
+
+  });
+
+
+
+
+  //store DOM elements
+  var imageWrapper = $('.cd-images-list'),
+    imagesList = imageWrapper.children('li'),
+    contentWrapper = $('.cd-content-block'),
+    contentList = contentWrapper.children('ul').eq(0).children('li'),
+    blockNavigation = $('.block-navigation'),
+    blockNavigationNext = blockNavigation.find('.cd-next'),
+    blockNavigationPrev = blockNavigation.find('.cd-prev'),
+    //used to check if the animation is running
+    animating = false;
+
+  //on mobile - open a single project content when selecting a project image
+  imageWrapper.on('click', 'a', function(event){
+    event.preventDefault();
+    var device = MQ();
+
+    (device == 'mobile') && updateBlock(imagesList.index($(this).parent('li')), 'mobile');
+  });
+
+  //on mobile - close visible project when clicking the .cd-close btn
+  contentWrapper.on('click', '.cd-close', function(){
+    var closeBtn = $(this);
+    if( !animating ) {
+      animating = true;
+
+      closeBtn.removeClass('is-scaled-up').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+        contentWrapper.removeClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+          animating = false;
+        });
+
+        $('.cd-image-block').removeClass('content-block-is-visible');
+        closeBtn.off('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend');
+      });
     }
+  });
 
-});
+  //on desktop - update visible project when clicking the .block-navigation
+  blockNavigation.on('click', 'button', function(){
+    var direction = $(this),
+      indexVisibleblock = imagesList.index(imageWrapper.children('li.is-selected'));
 
-$('.tab a').on('click', function (e) {
+    if( !direction.hasClass('inactive') ) {
+      var index = ( direction.hasClass('cd-next') ) ? (indexVisibleblock + 1) : (indexVisibleblock - 1);
+      updateBlock(index);
+    }
+  });
 
-  e.preventDefault();
+  //on desktop - update visible project on keydown
+  $(document).on('keydown', function(event){
+    var device = MQ();
+    if( event.which=='39' && !blockNavigationNext.hasClass('inactive') && device == 'desktop') {
+      //go to next project
+      updateBlock(imagesList.index(imageWrapper.children('li.is-selected')) + 1);
+    } else if( event.which=='37' && !blockNavigationPrev.hasClass('inactive') && device == 'desktop' ) {
+      //go to previous project
+      updateBlock(imagesList.index(imageWrapper.children('li.is-selected')) - 1);
+    }
+  });
 
-  $(this).parent().addClass('active');
-  $(this).parent().siblings().removeClass('active');
+  function updateBlock(n, device) {
+    if( !animating) {
+      animating = true;
+      var imageItem = imagesList.eq(n),
+        contentItem = contentList.eq(n);
 
-  target = $(this).attr('href');
+      classUpdate($([imageItem, contentItem]));
 
-  $('.tab-content > div').not(target).hide();
+      if( device == 'mobile') {
+        contentItem.scrollTop(0);
+        $('.cd-image-block').addClass('content-block-is-visible');
+        contentWrapper.addClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+          contentWrapper.find('.cd-close').addClass('is-scaled-up');
+          animating = false;
+        });
+      } else {
+        contentList.addClass('overflow-hidden');
+        contentItem.one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+          contentItem.siblings().scrollTop(0);
+          contentList.removeClass('overflow-hidden');
+          animating = false;
+        });
+      }
 
-  $(target).fadeIn(600);
+      //if browser doesn't support transition
+      if( $('.no-csstransitions').length > 0 ) animating = false;
+
+      updateBlockNavigation(n);
+    }
+  }
+
+  function classUpdate(items) {
+    items.each(function(){
+      var item = $(this);
+      item.addClass('is-selected').removeClass('move-left').siblings().removeClass('is-selected').end().prevAll().addClass('move-left').end().nextAll().removeClass('move-left');
+    });
+  }
+
+  function updateBlockNavigation(n) {
+    ( n == 0 ) ? blockNavigationPrev.addClass('inactive') : blockNavigationPrev.removeClass('inactive');
+    ( n + 1 >= imagesList.length ) ? blockNavigationNext.addClass('inactive') : blockNavigationNext.removeClass('inactive');
+  }
+
+  function MQ() {
+    return window.getComputedStyle(imageWrapper.get(0), '::before').getPropertyValue('content').replace(/'/g, "").replace(/"/g, "").split(', ');
+  }
 
 });
--- public.md
+++ public.md
@@ -15,14 +15,20 @@ sitemap:
 <div class="page--full">
   <div class="page--half public">
     <div class="section--content">
-      <h1>Public Library</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet!</p>
+      <h1>Public Library Features</h1>
+        <ul class="list-double">
+          <li><a href="">responsive</a></li>
+          <li><a href="">discovery</a></li>
+          <li><a href="">ILS integration</a></li>
+          <li><a href="">database portal</a></li>
+          <li><a href="">website CMS</a></li>
+        </ul>
+        <p><a href="/free-trial/" class="btn" data-type="page-transition">start free trial</a></p>
     </div>
   </div>
   <div class="page--half">
     <div class="page--quarter__top video--frame">
       <div class="section--content full--width">
-
           <video width="100%" controls>
             <source src="mov_bbb.mp4" type="video/mp4">
           </video>
@@ -31,16 +37,15 @@ sitemap:
     </div>
     <div class="page--quarter__bottom">
       <div class="section--content full--width">
-        <h2>Features</h2>
-        <ul class="list-double">
-          <li>responsive</li>
-          <li>discovery</li>
-          <li>ILS integration</li>
-          <li>database portal</li>
-          <li>website CMS</li>
-        </ul>
 
-        <p><a href="/free-trial/" class="btn" data-type="page-transition">start free trial</a></p>
+  {% include features.html %}
+        <p>Short description of public shit</p>
+
+        <div class="features">
+          <p>Experience your library anywhere, on any device!</p>
+          <p>Visually showcase desktop, tablet, phone</p>
+        </div>
+
       </div>
     </div>
   </div>
